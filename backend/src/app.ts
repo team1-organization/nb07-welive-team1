35770314b10641f298dde609071be5ceafdbbb48
gy <podojjang_kr@naver.com>
2026-05-11 15:16:23 +0900

    Feature/gy/deploy (#53)
    * feat: 프론트 URL 수정
    
    * feat: 프론트 URL 수정
    
    * feat: 프론트 URL 수정
    
    * feat: 프론트 URL 수정

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -54,7 +54,7 @@ app.use(
             }
         },
         credentials: true,
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     }),
 );
 
